b200bcae0f2109c1344483dd9d6af35741ab9c27
Ayush Priyadarshi <ayushpriya865@gmail.com>
2024-08-24 19:04:00 +0530

    Adding map/filter things
    along with readme

---
+++ index.js
@@ -0,0 +1,31 @@
+//map, arrow, filter functions 
+//function sum(a.b){
+//return (a+b);}=>
+// const sum=(a,b)=>{
+//     return (a+b);
+// }
+// console.log(sum(2,3))
+//difference between function and arrow function 
+
+//Given an array , give me back a new array in which every value is multiplied by 2
+arr=[1,2,3,4,5]
+// function transform(k){
+//     return k*2;
+// }
+// const ans=arr.map(transform);
+// console.log(ans)
+//Filtering
+//what if I tell u , given an input array ,give me back all the even values fromm it
+//ans
+// function filt(n){
+//     if(n%2==0){
+//         return true;
+//     }
+//     return false;
+// }
+// const ans=arr.filter(filt);
+// console.log(ans);
+//startsWith and endsWith
+
+str="hello"
+console.log(str.endsWith('o'))
